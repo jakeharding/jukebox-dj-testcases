f0d5d902cb848df13f84a5b1a86cfa6794e39502
Jake Harding <jacobeharding@gmail.com>
2017-11-22 17:11:28 -0600

    Rename Login test. Add manage event test.

---
+++ src/LoginTests.java
@@ -0,0 +1,71 @@
+/**
+ * LoginTests.java
+ * <p>
+ * Created by jake
+ * Created on 11/22/17
+ * <p>
+ * <p>
+ * Test the login page.
+ */
+
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import pages.LoginPage;
+
+public class LoginTests extends BaseTest {
+    private LoginPage page;
+    private String validUsername = "admin";
+    private String validPassword = "admin";
+
+    private String invalidUsername = "NoGoodUsername";
+    private String invalidPassword = "BadPassword";
+
+
+
+    @Before
+    public void setUp() throws Exception {
+        driver.get(baseUrl + "#/login");
+        page = new LoginPage(driver);
+    }
+
+    @Test
+    public void testLogin() throws Exception {
+        assertTrue(page.usernameInput.isVisible());
+        assertTrue(page.passwordInput.isVisible());
+
+        page.passwordInput.enter(validUsername);
+        page.usernameInput.enter(validPassword);
+
+        pause();
+        page.submit();
+        wait.until(page.hasRedirectedToDashboard());
+        pause();
+    }
+
+    @Test
+    public void testInvalidLogin () throws Exception {
+        assertTrue(page.usernameInput.isVisible());
+        assertTrue(page.passwordInput.isVisible());
+
+        page.passwordInput.enter(invalidPassword);
+        page.usernameInput.enter(invalidUsername);
+
+        pause();
+        page.submit();
+        wait.until(page.errorIsPresent());
+        pause();
+    }
+
+    @Test
+    public void testInvalidUsernameEntered () throws Exception {
+        page.usernameInput.enter("LongUsername");
+        page.usernameInput.clear();
+        page.passwordInput.clear();
+        pause();
+        wait.until(page.usernameErrorIsVisible());
+        pause();
+    }
+
+}
---
+++ src/ManageEventsTest.java
@@ -0,0 +1,13 @@
+/**
+ * ManageEventsTest.java
+ * <p>
+ * Created by jake
+ * Created on 11/22/17
+ * <p>
+ * <p>
+ * Test the DJ dashboard.
+ */
+
+
+public class ManageEventsTest {
+}
